1834a18fef93ca0f90e12e2e7913c670d91dfb3e
Christian Tellnes <christian@tellnes.no>
2014-06-11 19:51:38 +0200

    switch to pai_foot()

--- content/site.php
+++ content/site.php
@@ -59,8 +59,8 @@
   
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-  
-  	<?php pai_footer(); ?>
-  	
+
+    <?php pai_foot(); ?>
+
   </body>
 </html>
